ebcb57550d0acc18ba7e6ca3d6b781196a3f1dd7
Alex Tkachman <alex.tkachman@gmail.com>
2011-01-03 15:42:43 +0200

    static/non-static ternary/if variations added

--- src/main/groovy/FibGroovy.groovy
+++ src/main/groovy/FibGroovy.groovy
@@ -1,5 +1,17 @@
 class FibGroovy {
-    static int fib (int n) {
-        n >= 2 ? fib(n-1) + fib(n-2) : 1
+    static int fibStaticTernary (int n) {
+        n >= 2 ? fibStaticTernary(n-1) + fibStaticTernary(n-2) : 1
+    }
+
+    static int fibStaticIf (int n) {
+        if(n >= 2) fibStaticIf(n-1) + fibStaticIf(n-2) else 1
+    }
+
+    int fibTernary (int n) {
+        n >= 2 ? fibTernary(n-1) + fibTernary(n-2) : 1
+    }
+
+    int fibIf (int n) {
+        if(n >= 2) fibIf(n-1) + fibIf(n-2) else 1
     }
 }
--- src/main/groovy/Test.groovy
+++ src/main/groovy/Test.groovy
@@ -1,7 +1,19 @@
 for(i in 0..<10) {
     def start = System.currentTimeMillis()
-    FibGroovy.fib(40)
-    println("Groovy:  ${System.currentTimeMillis() - start}ms")
+    FibGroovy.fibStaticTernary(40)
+    println("Groovy(static ternary):  ${System.currentTimeMillis() - start}ms")
+
+    start = System.currentTimeMillis()
+    FibGroovy.fibStaticIf(40)
+    println("Groovy(static if):  ${System.currentTimeMillis() - start}ms")
+
+    start = System.currentTimeMillis()
+    new FibGroovy().fibTernary(40)
+    println("Groovy(instance ternary):  ${System.currentTimeMillis() - start}ms")
+
+    start = System.currentTimeMillis()
+    new FibGroovy().fibIf(40)
+    println("Groovy(instance if):  ${System.currentTimeMillis() - start}ms")
 
     start = System.currentTimeMillis()
     FibGroovypp.fib(40)
